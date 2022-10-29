f40c1e693e2ec44d0e79dbc8df15b3bb65fb649a
ihyun <okyee8335@naver.com>
2022-10-29 09:31:40 +0900

    10/29 ihyun feat)comments part

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const CommentsController = require('../controllers/comments.controller');
-const authAdminMiddleware = require('../middlewares/auth-middlewares');
+const authMiddleware = require('../middlewares/auth-middlewares');
 const commentsController = new CommentsController();
 
 router.get('/', commentsController.getComments);
-router.post('/', commentsController.createComments);
-router.put('/:commentsId', commentsController.updateComments);
-router.delete('/:commentsId', commentsController.deleteComments);
+router.post('/', authMiddleware, commentsController.createComments);
+router.put('/:commentsId', authMiddleware, commentsController.updateComments);
+router.delete('/:commentsId', authMiddleware, commentsController.deleteComments);
 
 module.exports = router;
