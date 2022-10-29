4ead02c7aa0137184269fbd6ffdea5fefd770f08
ihyun <okyee8335@naver.com>
2022-10-30 05:37:56 +0900

    10/30 ihyun Feat)ing..commentImage Upload

--- repositories/comments.repository.js
+++ repositories/comments.repository.js
@@ -1,7 +1,7 @@
 const {Comments} = require('../models/index');
 
 class CommentsRepository {
-    //댓글 목록 보기
+    //댓글 전체 목록 보기
     findAllComments = async (goodsId) => {
         const allCommentsData = await Comments.findAll({
             where: {goodsId},
@@ -10,21 +10,32 @@ class CommentsRepository {
         return allCommentsData;
     };
 
+    //댓글 한개 보기
+    findComment = async (userId, commentsId) => {
+        const findCommentData = await Comments.findOne({
+            where: {userId, commentsId}
+        });
+        return findCommentData;
+    }
+
     //신규 댓글
-    createComment = async(goodsId, id, userName, commentImage, content) => {
-        const createCommentData = await Comments.create({goodsId, id, userName, commentImage, content});
+    createComment = async(goodsId, userId, userName, commentImage, content) => {
+        const createCommentData = await Comments.create({goodsId, userId, userName, commentImage, content});
         return createCommentData;
     };
 
     //댓글 수정
-    updateComment = async(commentId, content) => {
-        const updatedCommentData = await Comments.update({content}, {where: {commentId}});
+    updateComment = async(userId, commentsId, content) => {
+        const updatedCommentData = await Comments.update({content}, {where: {userId, commentsId}});
         return updatedCommentData;
     };
 
     //댓글 삭제
-    deleteComment = async(commentId, userId) => {
-        const commentData = await Comments.findByPk(commentId, userId);
+    deleteComment = async(commentsId/*, userId*/) => {
+        const deleteCommentData = await Comments.destroy({
+            where: {commentsId/*, userId*/},
+        });
+        return deleteCommentData;
     };
 }
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const User = require('./user.route');
 const Goods = require('../routes/goods.route');
 const Carts = require('../routes/carts.route');
 const Comments = require('./comments.route');
-const Likes = require('../routes/likes.route');
+const Likes = require('./likes.route');
 
 
 // const commentsRouter = require('./comments.route');  //댓글
