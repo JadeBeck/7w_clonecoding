39b9df8c47ae8271ce7f1257b2c193901203c111
dahye24 <rhahsorkddkw@naver.com>
2022-10-31 17:29:19 +0900

    10/31 dahye likes에러처리중

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -5,36 +5,36 @@ class LikesRepository {
 
 //게시글 좋아요 누르기    
     uselikes = async (userId, goodsId) => {
-        // try {
+        try {
             const finduselikes = await Likes.findOne({where: {userId, goodsId}});
             return finduselikes;
-        // } catch {
-        //     const error = new Error(`서버 실행 중 오류가 발생했습니다.`);
-        //     error.statusCode = 500;
-        //     throw error;
-        // }
+        } catch {
+            const error = new Error(`서버 실행 중 오류가 발생했습니다.`);
+            error.statusCode = 500;
+            throw error;
+        }
     };
 
     updateLikes = async (userId, goodsId) => {
-        // try {}
+        try {
             const createlikes = await Likes.create({userId, goodsId});
             return createlikes;
-        // } catch {
-        //     const error = new Error(`좋아요 요청 중 오류가 발생했습니다.`);
-        //     error.statusCode = 500;
-        //     throw error;
-        // }
+        } catch {
+            const error = new Error(`찜 등록 요청 중 오류가 발생했습니다.`);
+            error.statusCode = 500;
+            throw error;
+        }
     };
 
     deletelikes = async (userId, goodsId) => {
-        // try {}
+        try {
             const deletelikes = await Likes.destroy({where: {userId, goodsId}});
             return deletelikes;
-        // } catch {
-        //     const error = new Error(`좋아요 취소 중 오류가 발생했습니다.`);
-        //     error.statusCode = 500;
-        //     throw error;
-        // }
+        } catch {
+            const error = new Error(`찜 등록 취소 중 오류가 발생했습니다.`);
+            error.statusCode = 500;
+            throw error;
+        }
     };
 
     // 찜 목록 조회
@@ -44,7 +44,7 @@ class LikesRepository {
             include: {
               model: Goods,
               key : 'goodsId',
-              attributes: ['goodsName', 'goodsImage', 'price'],}    
+              attributes: ['goodsName', 'goodsImage', 'price'],}    //goods랑 연결해서 필요한 부분만 빼오기
             
           });
           return likegoods;
