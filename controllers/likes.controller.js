f965b438182135426d20c8d6d65a10e5da1ecee6
dahye24 <rhahsorkddkw@naver.com>
2022-10-31 14:47:18 +0900

    10/31 dahye likes기능완료(미완성)

--- controllers/likes.controller.js
+++ controllers/likes.controller.js
@@ -6,14 +6,16 @@ class LikesController {
 //게시글 좋아요 누르기
 
 createLikes = async (req, res, next) => {
-//   try{
+  try{
       const { goodsId } = req.params;
       const { userId } = res.locals.user;
-      const updatelikes = await this.likesService.createLikes(goodsId, userId);
-      res.status(200).json({ message: updatelikes}); 
-//   }catch(err){
-    //   return res.status(err.statusCode || 500).json({message: err.message});
-//   } 
+      await this.likesService.createLikes(userId, goodsId);
+      res.status(201).json({ message });    //service에 적은 좋아요 등록메세지
+  }catch(cancel){  //catch 옆에 ()에 변수를 담아줘야한다고 한다.
+      return res.status(201).json({message: cancel.message});  //service에 적은 취소 등록메세지
+  } 
 }
+
 }
+
 module.exports = LikesController;
