98b4ee76a46657f57231b786959a7195d7f7f022
dahye24 <rhahsorkddkw@naver.com>
2022-11-02 13:36:40 +0900

    11/2 dahye login중복확인

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,9 +4,9 @@ const UserController = require('../controllers/user.controller');
 const userController = new UserController();
 
 // 회원 가입 controller
-//router.post('/users/signup', userController.createUser);
 router.post("/users/signup", userController.createUser);
-
+// 아이디 중복확인 controller
+router.post("/users/check", userController.checkID)
 // 로그인 controller
 router.post('/users/login', userController.loginUser);
 
